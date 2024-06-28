2600fff13461cd185b63c8b725e3f45de44e1a7a
srgbar <srg.bar@yandex.ru>
2024-06-28 18:49:55 +0300

    lesson 1 (props)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import './App.css';
+import TodoList, {TaskType} from "./TodoList";
 
 function App() {
+    // BLL
+    const todoListTitle_1: string = "What to learn"
+    const todoListTitle_2: string = "What to buy"
+    const tasks_1: Array<TaskType> = [
+        {id: 1, isDone: true, title: "HTML&CSS"},
+        {id: 2, isDone: true, title: "JS"},
+        {id: 3, isDone: false, title: "React"},
+        {id: 4, isDone: false, title: "Redux"},
+    ]
+    const tasks_2: Array<TaskType> = [
+        {id: 5, isDone: true, title: "Bread"},
+        {id: 6, isDone: false, title: "Chocolate"},
+        {id: 7, isDone: true, title: "Tea"},
+        {id: 8, isDone: false, title: "Milk"},
+    ]
+    // UI:
     return (
         <div className="App">
-            <div className="todolist">
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList
+                tasks={tasks_1}
+                title={todoListTitle_1}/>
+            <TodoList
+                tasks={tasks_2}
+                title={todoListTitle_2}/>
         </div>
     );
 }
